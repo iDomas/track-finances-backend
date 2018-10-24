c8ec46221bee10cd4e9298465e19ccccb0e6b307
iDomas <domo.emailas@gmail.com>
2018-10-24 20:44:48 +0300

    initial commit

---
+++ src/main/java/com/trackfinances/backend/trackfinancesbackend/TrackFinancesBackendApplication.java
@@ -0,0 +1,12 @@
+package com.trackfinances.backend.trackfinancesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFinancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFinancesBackendApplication.class, args);
+	}
+}
