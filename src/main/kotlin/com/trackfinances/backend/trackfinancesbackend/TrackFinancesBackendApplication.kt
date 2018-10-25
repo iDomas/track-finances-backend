fb0522ec0a063d04fe960fcac0f592d9c10c5fe1
iDomas <domo.emailas@gmail.com>
2018-10-25 20:26:27 +0300

    moved from java to kotlin

---
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/TrackFinancesBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.trackfinances.backend.trackfinancesbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackFinancesBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrackFinancesBackendApplication>(*args)
+}
