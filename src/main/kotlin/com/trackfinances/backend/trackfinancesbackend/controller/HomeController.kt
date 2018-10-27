3ca279c74f97c7b164615732a3a40a97c0904916
iDomas <domo.emailas@gmail.com>
2018-10-27 11:27:08 +0300

    home controller, added web dependency

---
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/HomeController.kt
@@ -0,0 +1,16 @@
+package com.trackfinances.backend.trackfinancesbackend.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    fun home(): String {
+        return "Welcome!"
+    }
+
+}
