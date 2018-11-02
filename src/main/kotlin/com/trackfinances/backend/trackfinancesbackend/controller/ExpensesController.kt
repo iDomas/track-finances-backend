69ab8bbdda86581b1b75d96bfc655530cb81a59f
iDomas <domo.emailas@gmail.com>
2018-11-02 12:37:58 +0200

    Insert expenses and get all expenses related to username  and userId

--- src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
@@ -3,21 +3,31 @@ package com.trackfinances.backend.trackfinancesbackend.controller
 import com.trackfinances.backend.trackfinancesbackend.model.Expenses
 import com.trackfinances.backend.trackfinancesbackend.repository.ExpensesRepository
 import com.trackfinances.backend.trackfinancesbackend.repository.UsersRepository
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/expenses")
 class ExpensesController(private val expensesRepository: ExpensesRepository, private val usersRepository: UsersRepository) {
 
+
     @GetMapping(value = ["","/"])
     @ResponseBody
     fun allExpenses(): List<Expenses> {
-        return expensesRepository.findAll().iterator().asSequence().toList();
+        val user: Any = SecurityContextHolder.getContext().authentication.principal
+        val userId: Long = usersRepository.findByUsername(user.toString()).id
+        return expensesRepository.getAllByUserId(userId);
     }
 
     @PostMapping(value = ["","/"])
     @ResponseBody
     fun insertExpense(@RequestBody expenses: Expenses): Expenses {
+        val user: Any = SecurityContextHolder.getContext().authentication.principal
+        val userId: Long = usersRepository.findByUsername(user.toString()).id
+        expenses.userId = userId
+        println("Expense: " + expenses)
         expensesRepository.save(expenses);
         return expenses;
     }
--- src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/repository/ExpensesRepository.kt
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/repository/ExpensesRepository.kt
@@ -4,4 +4,5 @@ import com.trackfinances.backend.trackfinancesbackend.model.Expenses
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ExpensesRepository : JpaRepository<Expenses, Long> {
+    fun getAllByUserId(userId: Long): List<Expenses>
 }
