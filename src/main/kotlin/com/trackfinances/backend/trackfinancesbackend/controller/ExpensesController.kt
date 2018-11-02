674e0c705628304baac6a5ba1eb4d199edc7579b
iDomas <domo.emailas@gmail.com>
2018-11-02 13:18:33 +0200

    Get expense by id

--- src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
@@ -3,6 +3,7 @@ package com.trackfinances.backend.trackfinancesbackend.controller
 import com.trackfinances.backend.trackfinancesbackend.model.Expenses
 import com.trackfinances.backend.trackfinancesbackend.repository.ExpensesRepository
 import com.trackfinances.backend.trackfinancesbackend.repository.UsersRepository
+import javassist.NotFoundException
 import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
@@ -27,9 +28,25 @@ class ExpensesController(private val expensesRepository: ExpensesRepository, pri
         val user: Any = SecurityContextHolder.getContext().authentication.principal
         val userId: Long = usersRepository.findByUsername(user.toString()).id
         expenses.userId = userId
-        println("Expense: " + expenses)
         expensesRepository.save(expenses);
         return expenses;
     }
+
+    @GetMapping(value = ["/{id}", "/{id}/"])
+    @ResponseBody
+    fun getExpenseById(@PathVariable("id") id: Long): Expenses {
+        val user: Any = SecurityContextHolder.getContext().authentication.principal
+        val userId: Long = usersRepository.findByUsername(user.toString()).id
+
+        val expense: Expenses = expensesRepository.findById(id).get()
+
+        if (expense.userId != userId) {
+            throw NotFoundException("expense.userId != userId")
+        }
+
+        return expense
+    }
+
+
 
 }
