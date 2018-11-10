a744d788ff37c1c495387a3881d68628ba44bdf6
iDomas <domo.emailas@gmail.com>
2018-11-10 21:04:53 +0200

    some tweak, token expiration works properly

--- src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/security/SecurityConstants.kt
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/security/SecurityConstants.kt
@@ -5,7 +5,7 @@ class SecurityConstants (
 ) {
     companion object {
         const val SECRET: String = "SecretKeyToGenJWTs"
-        const val EXPIRATION_TIME: Long = 3_600
+        const val EXPIRATION_TIME: Long = 86_400_000
         const val TOKEN_PREFIX: String = "Bearer "
         const val HEADER_STRING: String = "Authorization"
         const val SIGN_UP_URL: String = "/users"
