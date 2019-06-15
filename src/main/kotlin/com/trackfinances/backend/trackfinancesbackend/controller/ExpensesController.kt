ddf4fb0eabaa4aecf8224b0dbb1321d6f2c3ae5c
iDomas <domo.emailas@gmail.com>
2019-06-15 19:47:37 +0300

    Refactored to use ExpensesService

--- src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
+++ src/main/kotlin/com/trackfinances/backend/trackfinancesbackend/controller/ExpensesController.kt
@@ -1,81 +1,43 @@
 package com.trackfinances.backend.trackfinancesbackend.controller
 
 import com.trackfinances.backend.trackfinancesbackend.model.Expenses
-import com.trackfinances.backend.trackfinancesbackend.model.Users
-import com.trackfinances.backend.trackfinancesbackend.repository.ExpensesRepository
-import com.trackfinances.backend.trackfinancesbackend.repository.UsersRepository
-import org.springframework.security.core.context.SecurityContextHolder
+import com.trackfinances.backend.trackfinancesbackend.service.ExpensesService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/expenses")
-class ExpensesController(private val expensesRepository: ExpensesRepository, private val usersRepository: UsersRepository) {
+class ExpensesController(@Autowired private val expensesService: ExpensesService) {
 
 	@GetMapping(value = ["", "/"])
 	@ResponseBody
 	fun allExpensesByUser(): List<Expenses> {
-		val user: Any = SecurityContextHolder.getContext().authentication.principal
-		val currentUser: Users = usersRepository.findByUsername(user.toString())
-
-		val expenses = expensesRepository.findAllExpensesByUsers(currentUser)
-
-		if (expenses.isEmpty())
-			return emptyList()
-
-		return expenses
+		return expensesService.allExpensesByUser()
 	}
 
 	@PostMapping(value = ["", "/"])
 	@ResponseBody
 	fun insertExpense(@RequestBody expenses: Expenses): Expenses {
-		val user: Any = SecurityContextHolder.getContext().authentication.principal
-		expenses.users = usersRepository.findByUsername(user.toString())
-
-		expensesRepository.save(expenses)
-
-		return expenses
+		return expensesService.insertExpense(expenses)
 	}
 
 	@GetMapping(value = ["/{expenseId}", "/{expenseId}/"])
 	@ResponseBody
 	fun getExpenseById(@PathVariable("expenseId") expenseId: Long): Expenses {
-		val user: Any = SecurityContextHolder.getContext().authentication.principal
-		val currentUser: Users = usersRepository.findByUsername(user.toString())
-
-		val expense: Expenses = expensesRepository.findById(expenseId).get()
-		val expensesUserId: Long? = expense.users?.id
-
-		if (currentUser.id != expensesUserId)
-			throw Exception("NO SUCH USER AND EXPENSE WITH USER_ID: $currentUser.id AND EXPENSE_ID: $expensesUserId")
-
-		return expense
+		return expensesService.getExpenseById(expenseId)
 	}
 
 	@PutMapping(value = ["/{expenseId}", "/{expenseId}/"])
 	@ResponseBody
 	fun updateExpense(@RequestBody expenses: Expenses,
 					  @PathVariable("expenseId") expenseId: Long): Expenses {
-		val user: Any = SecurityContextHolder.getContext().authentication.principal
-		val currentUser: Users = usersRepository.findByUsername(user.toString())
-
-		val expense: Expenses = expensesRepository.findById(expenseId).get()
-		val expensesUserId: Long? = expense.users?.id
-
-		if (currentUser.id != expensesUserId)
-			throw Exception("NO SUCH USER AND EXPENSE WITH USER_ID: $currentUser.id AND EXPENSE_ID: $expensesUserId")
-
-		expense.price = expenses.price
-		expense.title = expenses.title
-		expense.description = expenses.description
-
-		return expensesRepository.save(expense)
+		return expensesService.updateExpense(expenses, expenseId)
 	}
 
 	@DeleteMapping(value = ["/{expenseId}", "/{expenseId}/"])
 	@ResponseBody
 	fun deleteExpense(@PathVariable("expenseId") expenseId: Long): String {
-		expensesRepository.deleteById(expenseId)
-		return "{\"status\":\"OK\"}"
+		return expensesService.deleteExpense(expenseId)
 	}
 
 }
